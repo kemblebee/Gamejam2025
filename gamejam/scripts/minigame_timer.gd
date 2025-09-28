0a6c83a364332b48fce1526193248938f699dee3
Kem <36427661+kemblebee@users.noreply.github.com>
2025-09-28 11:10:02 -0500

    Timer & Typing Game Made

--- gamejam/scripts/minigame_timer.gd
+++ gamejam/scripts/minigame_timer.gd
@@ -3,9 +3,12 @@ extends Node
 #Lowers the percentage on the bar by 1 each second that passes.
 func _on_timer_timeout() -> void:
 	$ProgressBar.value -= 1
+	
+	#Do something when the progress bar hits 0
+	if $ProgressBar.value == 0:
+		pass
 
 
-
-func _on_node_test_sig() -> void:
+func _on_typing_checker_word_correct() -> void:
 	$ProgressBar.value += 2
-	$AffBar.value += 2
+	$AffBar.value += 5
--- gamejam/scripts/typinggame.gd
+++ gamejam/scripts/typinggame.gd
@@ -1,11 +1,33 @@
 extends Control
 
-
-@onready var display_text = $DisplayLabel.text
-@onready var write_text = $WriteLabel.text
-
 signal word_correct
 
-func change_label():
-	if display_text == write_text:
+func _on_line_edit_text_changed(new_text: String) -> void:
+	#new_text is the FULL STRING DO NOT GET THIS INCORRECT!
+	
+	#Labelling these here cause I reuse these a lot
+	var disp_low = $DisplayLabel.text.to_lower()
+	var new_low = new_text.to_lower()
+	
+	#If everything is correct: emit word_correct and print WOO
+	if disp_low == new_low:
 		word_correct.emit()
+		print("WOO")
+		
+	#If full word isn't correct, check if current length is correct
+	else:
+		#Substring to the current length
+		#Move this later so it can be done when initalized and not every time
+		var disp_cur = disp_low.substr(0, new_low.length())
+		
+		#Check here
+		if disp_cur == new_low:
+			print("Curr Correct!")
+		else:
+			print("ew")
+
+#?? Old legacy input when I didn't know LineEdit existed
+#func _input(event):
+	#if event is InputEventKey and event.pressed:
+		#print(event.as_text())
+		#$WriteLabel.text += event.as_text()
